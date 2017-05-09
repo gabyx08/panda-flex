30fbcdc464d9c24283e39745ecae970d0f6b5610
fbk-rena <gaby.ranarz@gmail.com>
2017-05-09 16:03:02 -0500

    funcionalidad botones origen y extincion

--- js/javascript.js
+++ js/javascript.js
@@ -1,12 +1,18 @@
 function iniciarEventos(){
-  // click al btn x (cerrar imagenes pandas)
+  // click al btn x (cerrar imagenes de pandas)
   var btnCerrar = document.getElementsByClassName("cerrar");
-  for(var i=0; i<btnCerrar.length;i++){
+  for(var i  = 0; i < btnCerrar.length; i++){
     btnCerrar[i].addEventListener("click", quitarImagen);
   }
   // click btn restaurar fotos
   var btnRestaurar = document.getElementById("restaurarBtn");
   btnRestaurar.addEventListener("click", restaurarImagenes);
+  //click btn origen
+  var btnOrigen = document.getElementById("btnOrigen");
+  btnOrigen.addEventListener("click", mostrarOcultarOrigen);
+  //click btn extincion
+  var btnExtincion = document.getElementById("btnExtincion");
+  btnExtincion.addEventListener("click", mostrarOcultarExtincion);
 }
 
 function quitarImagen(){
@@ -17,7 +23,25 @@ function quitarImagen(){
 function restaurarImagenes(){
   var imagenes= document.getElementsByClassName("contenedorImagen");
   for(var j=0; j<imagenes.length; j++){
-    imagenes[j].style.display="block";
+    imagenes[j].style.display = "block";
+  }
+}
+
+function mostrarOcultarOrigen(){
+  var txtOrigen = document.getElementById("origen");
+  if (txtOrigen.style.visibility === "hidden"){
+    txtOrigen.style.visibility = "visible";
+  }else{
+    txtOrigen.style.visibility = "hidden";
+  }
+}
+
+function mostrarOcultarExtincion(){
+  var txtExtincion = document.getElementById("extincion");
+  if (txtExtincion.style.visibility === "hidden"){
+    txtExtincion.style.visibility = "visible";
+  }else{
+    txtExtincion.style.visibility = "hidden";
   }
 }
 
